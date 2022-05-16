e5287cf908b88f073e86f330d80b670452726baf
sonkeehoon <81700507+sonkeehoon@users.noreply.github.com>
2022-05-16 19:32:56 +0900

    Update WordCloud.R

--- WordCloud.R
+++ WordCloud.R
@@ -1,11 +1,9 @@
-#필요 라이브러리 불러오기
 library(readr)
 library(tidytext)
 library(dplyr)
 
 raw_data= read_lines(file.path
-          ("C:","Users","user0401","Documents","R",
-          "KakaoTalk_20220123_1501_12_572_Sister1.txt"))
+          ("C:\\Users\\user0401\\Desktop\\PythonWorkspace\\coding_test\\Coding_test\\법성포단오제.txt"))
 raw_data <- data.frame(line = 1:length(raw_data),
                         text = raw_data, 
                         stringsAsFactors = FALSE)
@@ -17,10 +15,8 @@ tidyToken <- tokenData %>%
 
 tidyToken <- tidyToken %>%
    count(word, sort = TRUE)
-head(tidyToken) #TOP3 출력
+head(tidyToken)
 library(wordcloud)
 wordcloud(words = tidyToken$word, freq = tidyToken$n, min.freq = 2, max.words = 200, 
           random.order = FALSE, rot.per = 0.35,
           colors = brewer.pal(8, "Dark2"))
-
-
